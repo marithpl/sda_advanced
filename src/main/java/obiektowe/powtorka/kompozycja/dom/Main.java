dd14957e60da2b9ea66c4d88aeec6d4d26fb8d33
Damian Mury <damianmury@MacBook-Pro-Damian-2.local>
2022-06-28 19:59:27 +0200

    next tasks

--- src/main/java/obiektowe/powtorka/kompozycja/dom/Main.java
+++ src/main/java/obiektowe/powtorka/kompozycja/dom/Main.java
@@ -4,31 +4,12 @@ import obiektowe.powtorka.samochod.Samochod;
 
 public class Main {
     public static void main(String[] args) {
-//        Window[] WindowList = {
-//                new Window(),
-//                new Window(),
-//                new Window(),
-//                new Window()
-//        };
-//
-//        Bed[] BedList = {
-//                new Bed(),
-//                new Bed(),
-//                new Bed()
-//        };
 
         Room[] RoomList = {
           new Room(),
           new Room()
         };
 
-//        for (Window okno : WindowList) {
-//            System.out.println(okno.toString());
-//        }
-//
-//        for (Bed lozko : BedList) {
-//            System.out.println(lozko.toString());
-//        }
         for (Room pokoj : RoomList) {
             System.out.println(pokoj.toString());
         }
