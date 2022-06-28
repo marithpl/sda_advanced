fa2d86234afd4c24116b29900aaed22cd4db91f2
Damian Mury <damianmury@MacBook-Pro-Damian-2.local>
2022-06-28 19:57:40 +0200

    next tasks

--- src/main/java/obiektowe/powtorka/kompozycja/dom/Main.java
+++ src/main/java/obiektowe/powtorka/kompozycja/dom/Main.java
@@ -4,25 +4,33 @@ import obiektowe.powtorka.samochod.Samochod;
 
 public class Main {
     public static void main(String[] args) {
-        Window[] WindowList = {
-                new Window(),
-                new Window(),
-                new Window(),
-                new Window()
+//        Window[] WindowList = {
+//                new Window(),
+//                new Window(),
+//                new Window(),
+//                new Window()
+//        };
+//
+//        Bed[] BedList = {
+//                new Bed(),
+//                new Bed(),
+//                new Bed()
+//        };
+
+        Room[] RoomList = {
+          new Room(),
+          new Room()
         };
 
-        Bed[] BedList = {
-                new Bed(),
-                new Bed(),
-                new Bed()
-        };
-
-        for (Window okno : WindowList) {
-            System.out.println(okno.toString());
-        }
-
-        for (Bed lozko : BedList) {
-            System.out.println(lozko.toString());
+//        for (Window okno : WindowList) {
+//            System.out.println(okno.toString());
+//        }
+//
+//        for (Bed lozko : BedList) {
+//            System.out.println(lozko.toString());
+//        }
+        for (Room pokoj : RoomList) {
+            System.out.println(pokoj.toString());
         }
     }
 }
--- src/main/java/obiektowe/powtorka/kompozycja/dom/Room.java
+++ src/main/java/obiektowe/powtorka/kompozycja/dom/Room.java
@@ -1,5 +1,21 @@
 package obiektowe.powtorka.kompozycja.dom;
 
+import java.util.Arrays;
+
 public class Room {
     private Bed bed;
+    private Window[] windows;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "bed=" + bed +
+                ", windows=" + Arrays.toString(windows) +
+                '}';
+    }
+
+    public Room() {
+        this.bed = new Bed();
+        this.windows = new Window[]{ new Window(), new Window()};
+    }
 }
